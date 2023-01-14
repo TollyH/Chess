49c77c769712556c06dee255f90651a31be094ef
Tolly <34184902+TollyH@users.noreply.github.com>
2023-01-14 17:12:36 +0000

    Use parameter references in AfterMove doc

--- Extensions.cs
+++ Extensions.cs
@@ -5,7 +5,7 @@ namespace Chess
     public static class Extensions
     {
         /// <summary>
-        /// Get a copy of the given board with the piece at oldPoint moved to newPoint.
+        /// Get a copy of the given board with the piece at <paramref name="oldPoint"/> moved to <paramref name="newPoint"/>.
         /// </summary>
         public static Pieces.Piece?[,] AfterMove(this Pieces.Piece?[,] board, Point oldPoint, Point newPoint)
         {
